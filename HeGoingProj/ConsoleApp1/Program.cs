8a1784f3fc6c5aefe01f7314649dd24f063c87f9
LegendaryLi <cofgtweart@gmail.com>
2022-01-31 15:22:37 +0500

    added func div

--- HeGoingProj/ConsoleApp1/Program.cs
+++ HeGoingProj/ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void div()
+{
+    Console.WriteLine(int.Parse(EntrA()) / int.Parse(EntrB()));
+}
