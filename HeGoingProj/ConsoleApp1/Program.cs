87a5c922d1e7f5c87f1dcf30a8376ffca147201d
Viktorrrr <viktor.kononenko.2003@gmail.com>
2022-01-31 15:20:15 +0500

    added func in_a

--- HeGoingProj/ConsoleApp1/Program.cs
+++ HeGoingProj/ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string EntrA()
+{
+    Console.WriteLine("Введите а");
+    return Console.ReadLine();
+}
