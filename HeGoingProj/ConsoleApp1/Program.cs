0bcdf6b2ded9746d3d0a350d7a62108f1448fcd8
Viktorrrr <viktor.kononenko.2003@gmail.com>
2022-01-31 15:43:02 +0500

    added first msg in master

--- HeGoingProj/ConsoleApp1/Program.cs
+++ HeGoingProj/ConsoleApp1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Начало работы программы");
+int a = int.Parse(EntrA());
+int b = int.Parse(EntrB());
+sum();
+sub();
+mult();
+div();
